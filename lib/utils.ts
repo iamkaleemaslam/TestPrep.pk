0a21aae1142a9ee6fee809661a3c85871137af52
iamkaleemaslam <iamkaleemaslam@gmail.com>
2026-04-22 22:45:34 +0500

    Add files via upload

---
+++ lib/utils.ts
@@ -0,0 +1,17 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatPercentage(value: number) {
+  return `${Math.round(value)}%`;
+}
+
+export function slugify(input: string) {
+  return input
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/(^-|-$)+/g, '');
+}
